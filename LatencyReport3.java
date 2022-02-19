0e989a4d9ccadbfd3fcc1d22a9b0871b817b274f
Viral Thakkar <viralthakkar86@gmail.com>
2022-02-19 12:32:56 +0530

    Add files via upload

---
+++ LatencyReport3.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class LatencyReport3 {
+
+
+	public static void main(String[] args) throws IOException,
+	InterruptedException {
+		List<String> retransmissionByTimeFrame = new ArrayList<>();
+		Integer totalRetransmission = null;
+		List<String> Latency = new ArrayList<>();
+		Map<Double, Double> LatencyMap = new TreeMap<>();
+
+		String rtt = "tshark -Tfields -E header=y -e frame.time_relative -e tcp.analysis.ack_rtt -e tcp.analysis.retransmission -r C:\\Java-Test-Example\\test.pcap";
+		Process process3 = Runtime.getRuntime().exec(rtt);
+		BufferedReader stdInput3 = new BufferedReader(new InputStreamReader(
+				process3.getInputStream()));
+		// Read the output from the command
+		System.out.println("Here is the standard output of the command:\n");
+		String s3 = null;
+		int skipFirstLine=0;
+		while ((s3 = stdInput3.readLine()) != null) {
+			if(skipFirstLine>0){
+				String[] aa = s3.split("\\s+");
+				if((!"".equals(aa[0])) && aa.length==2 && !"".equals(aa[1]) ){
+					LatencyMap.put(Double.parseDouble(aa[0]),
+							Double.parseDouble(aa[1]));	
+				}else{
+					LatencyMap.put(Double.parseDouble(aa[0]),
+							0D);
+				}	
+			}
+
+			skipFirstLine++;
+		}
+		
+		int i = 5;
+		Map<Integer, List<Double>> xx = new TreeMap<>();
+		for (Map.Entry<Double, Double> item1 : LatencyMap.entrySet()) {
+			if (item1.getKey() < i) {
+				xx.computeIfAbsent(i, k -> new ArrayList<>()).add(item1.getValue());
+			} else {
+				i = i + 5;
+			}
+		}
+
+		System.out.println(LatencyMap.size());
+		System.out.println(xx.size());
+		xx.forEach((k,v)->{
+			System.out.println();
+			System.out.println("Key time "+k +" Size "+ v.size());
+			
+		});
+		
+		for (Map.Entry<Integer, List<Double>> item1 : xx.entrySet()) {
+				System.out.println(item1.getValue().stream().mapToDouble(f -> f.doubleValue()).sum());
+		}
+
+	}
+
+
+}
