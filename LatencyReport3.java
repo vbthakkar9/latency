76867fa53be9110b24af40b3753d96f7549f56e4
Viral Thakkar <viralthakkar86@gmail.com>
2022-02-19 17:13:11 +0530

    Update LatencyReport3.java

--- LatencyReport3.java
+++ LatencyReport3.java
@@ -1,29 +1,54 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-
-public class LatencyReport3 {
-
-
+public class LatencyReportGenerator {
 	public static void main(String[] args) throws IOException,
 	InterruptedException {
 		List<String> retransmissionByTimeFrame = new ArrayList<>();
 		Integer totalRetransmission = null;
-		List<String> Latency = new ArrayList<>();
-		Map<Double, Double> LatencyMap = new TreeMap<>();
+		Map<Double,Double> LatencyMap = new HashMap<>();
+		String retransmissionByTimeFrameCommand = "tshark -r test.pcap -q -z io,stat,5,\"COUNT(tcp.analysis.retransmission) tcp.analysis.retransmission\"| grep -P \"\\d+\\.?\\d*\\s+<>\\s+|Interval +\\|\" | tr \"|\" \" \" | sed -E 's/<>/-/;'";
 
-		String rtt = "tshark -Tfields -E header=y -e frame.time_relative -e tcp.analysis.ack_rtt -e tcp.analysis.retransmission -r C:\\Java-Test-Example\\test.pcap";
-		Process process3 = Runtime.getRuntime().exec(rtt);
-		BufferedReader stdInput3 = new BufferedReader(new InputStreamReader(process3.getInputStream()));
+		Process process = Runtime.getRuntime().exec(
+				retransmissionByTimeFrameCommand);
+
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+				process.getInputStream()));
+
 		// Read the output from the command
-		System.out.println("Here is the standard output of the command:\n");
+		//System.out.println("Here is the standard output of the command:\n");
+		String s = null;
+		while ((s = stdInput.readLine()) != null) {
+			retransmissionByTimeFrame.add(s);
+		}
+
+		String totalRetransmissionCommnd = "tshark -n -r test.pcap -Y \"tcp.analysis.retransmission\" -T fields -e tcp.stream | wc -l";
+		Process process2 = Runtime.getRuntime().exec(totalRetransmissionCommnd);
+
+		BufferedReader stdInput2 = new BufferedReader(new InputStreamReader(
+				process2.getInputStream()));
+
+		// Read the output from the command
+		//System.out.println("Here is the standard output of the command:\n");
+		String s2 = null;
+		while ((s2 = stdInput2.readLine()) != null) {
+			totalRetransmission = Integer.parseInt(s2);
+		}
+
+		String rtt = "tshark -Tfields -E header=y -e frame.time_relative -e tcp.analysis.ack_rtt -r test.pcap";
+		Process process3 = Runtime.getRuntime().exec(rtt);
+		BufferedReader stdInput3= new BufferedReader(new InputStreamReader(
+				process3.getInputStream()));
+		// Read the output from the command
+		//System.out.println("Here is the standard output of the command:\n");
 		String s3 = null;
 		int skipFirstLine=0;
 		while ((s3 = stdInput3.readLine()) != null) {
@@ -36,7 +61,7 @@ public class LatencyReport3 {
 
 			skipFirstLine++;
 		}
-		
+
 		int i = 5;
 		Map<Integer, List<Double>> xx = new TreeMap<>();
 		for (Map.Entry<Double, Double> item1 : LatencyMap.entrySet()) {
@@ -46,29 +71,20 @@ public class LatencyReport3 {
 				i = i + 5;
 			}
 		}
-
-		System.out.println(LatencyMap.size());
-		System.out.println(xx.size());
-		xx.forEach((k,v)->{
-			System.out.println();
-			System.out.println("Key time "+k +" Size "+ v.size());
-			
-		});
-		
 		for (Map.Entry<Integer, List<Double>> item1 : xx.entrySet()) {
 			item1.getValue().sort(Comparator.naturalOrder());
-		}
-		
-		for (Map.Entry<Integer, List<Double>> item1 : xx.entrySet()) {
 			double average  = item1.getValue().stream().mapToDouble(f -> f).average().getAsDouble();
-			System.out.println("Before Size" + item1.getValue().size());
+			//System.out.println("Before Size" + item1.getValue().size());
 			int index  = (item1.getValue().size()*95)/100;
 			double p95  =item1.getValue().stream().mapToDouble(f -> f).limit(index).average().getAsDouble();
-			System.out.println("After Size" + item1.getValue().stream().mapToDouble(f -> f).limit(index).count());
-			System.out.println("Average "+ average + " p95 "+p95);
+			//System.out.println("After Size" + item1.getValue().stream().mapToDouble(f -> f).limit(index).count());
+			System.out.println("Latency Average:  "+ average + "    p95:  "+p95);
 		}
 
-	}
+		System.out.println("Retransmission by every 5 second: "
+				+ retransmissionByTimeFrame);
+		System.out.println("Total Retransmission: " + totalRetransmission);
 
+	}
 
 }
